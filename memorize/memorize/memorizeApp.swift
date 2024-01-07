1a1c5eeaeeba455e08edffed20df9bba39eaa112
Hello World <helloworld@example.com>
2024-01-07 17:27:07 -0500

    Create xcode files

---
+++ memorize/memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Hello World on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
